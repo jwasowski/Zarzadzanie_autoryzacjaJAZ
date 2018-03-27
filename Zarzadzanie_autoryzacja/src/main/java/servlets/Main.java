317e3101fd8ac069c5f786a424e61118127ecd0a
jwasowski <kuba.wasowski@mixbox.pl>
2018-03-27 16:46:01 +0200

    Rozpoczecie prac

---
+++ Zarzadzanie_autoryzacja/src/main/java/servlets/Main.java
@@ -0,0 +1,14 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Main {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		
+	}
+}
